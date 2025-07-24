ef0698fc0e0cd26d90d7c49383667eb02bf0c261
ArnavSaikia <arnavsaikia817@gmail.com>
2025-07-24 22:23:20 +0530

    setting barebones index page

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.listen(3000);
+
+app.get('/', (req , res) => {
+    // res.send("kid named finger");
+    res.render('home');
+})
