aaa9ee31b3b7766bef2a3e9a8441ea5da0930fdc
ArnavSaikia <arnavsaikia817@gmail.com>
2025-07-25 16:01:36 +0530

    mongoose set up. need to add some template entries in the database first

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const dbURI = 'mongodb+srv://test-user:chinesedog555@cluster01.kyhywfy.mongodb.net/wallpaper-gallery?retryWrites=true&w=majority&appName=Cluster01';
+mongoose.connect(dbURI)
+.then(() => app.listen(3000))
+.catch(err => console.log(err));
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
-app.listen(3000);
 
 app.get('/', (req , res) => {
     // res.send("kid named finger");
